28c6a5d84a6485f928cc158fe409a43855a1b724
Shirin Mohajer <shirinm@Shirins-MacBook-Pro.local>
2022-03-29 20:32:06 -0400

    add -= *= /= oporators and skip character

--- javascript-challenge/js-basic-challenge.js
+++ javascript-challenge/js-basic-challenge.js
@@ -1,4 +1,31 @@
 //This is a practice JavaScript code
+//when you need a literal quote: " or ' inside of your string
+/**
+ * In JavaScript, you can escape a quote from considering 
+ * it as an end of string quote by placing a backslash (\) in front of the quote.
+ */
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+//#5 Convert the assignments for a, b, and c to use the /= operator.
+let ax = 48;
+let bx = 108;
+let cx = 33;
+ax /= 12;
+bx /= 4;
+cx /= 11;
+//#4 Convert the assignments for a, b, and c to use the *= operator.
+let aaa = 5;
+let bbb = 12;
+let ccc = 4.6;
+aaa *= 5;
+bbb *= 3;
+ccc *= 10;
+//#3 Convert the assignments for a, b, and c to use the -= operator.
+let aa = 11;
+let bb = 9;
+let cc = 3;
+aa -= 6;
+bb -= 15;
+cc -= 1;
 //#2 Convert the assignments for a, b, and c to use the += operator.
 let a = 3;
 let b = 17;
