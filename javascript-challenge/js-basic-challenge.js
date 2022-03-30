5924d9b709588a485420fca621577ef2221f0c41
Shirin Mohajer <shirinm@Shirins-MacBook-Pro.local>
2022-03-29 20:12:47 -0400

    add var vs let

--- javascript-challenge/js-basic-challenge.js
+++ javascript-challenge/js-basic-challenge.js
@@ -1,1 +1,8 @@
 //This is a practice JavaScript code
+/* *
+ * var keyword can be easily overwritten,
+ * Unlike var, when you use let, a variable with the same name can only be declared once.
+ * It is common for developers to use uppercase variable 
+ * identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays). 
+ * 
+*/
