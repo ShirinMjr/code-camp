3b892d974923e2d4dc6abda7a8d9924e2b464b6d
Shirin Mohajer <shirinm@Shirins-MacBook-Pro.local>
2022-03-29 20:22:07 -0400

    += sign

--- javascript-challenge/js-basic-challenge.js
+++ javascript-challenge/js-basic-challenge.js
@@ -1,8 +1,15 @@
 //This is a practice JavaScript code
-/* *
+/* * #1
  * var keyword can be easily overwritten,
  * Unlike var, when you use let, a variable with the same name can only be declared once.
- * It is common for developers to use uppercase variable 
- * identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays). 
- * 
+ * It is common for developers to use uppercase variable
+ * identifiers for immutable values and lowercase or camelCase for mutable values (objects and arrays).
+ *
 */
+//#2 Convert the assignments for a, b, and c to use the += operator.
+let a = 3;
+let b = 17;
+let c = 12;
+a += 12;
+b += 9;
+c += 7;
