3bf4530ef3954b6eaebea94ae4b361395e7b772b
Shirin Mohajer <shirinm@Shirins-MacBook-Pro.local>
2022-05-04 08:27:47 -0400

    recursion count down project desc

---
+++ javascript-challenge/recursion-to-Create-a-Countdown.js
@@ -0,0 +1,17 @@
+/**
+ * function that returns an array of consecutive integers
+ * starting with 1 through the number passed to the function.
+ * There will be a base case. The base case tells the recursive function when it no longer needs to call
+ * itself. It is a simple case where the return value is already known.
+ * There will also be a recursive call which executes the original function with different arguments.
+ * If the function is written correctly, eventually the base case will be reached.
+ * 
+ * We have defined a function called countdown with one parameter (n).
+ * The function should use recursion to return an array containing the integers n through 1 
+ * based on the n parameter.
+ * If the function is called with a number less than 1, t
+ * he function should return an empty array.
+ * For example, calling this function with n = 5 
+ * should return the array [5, 4, 3, 2, 1]. 
+ * Your function must use recursion by calling itself and must not use loops of any kind.
+ */
