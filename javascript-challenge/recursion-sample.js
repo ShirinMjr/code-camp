ba332d4584531e4e015c577c48cb09ccce52a83d
Shirin Mohajer <shirinm@Shirins-MacBook-Pro.local>
2022-05-16 11:45:46 -0400

    add factorial function

--- javascript-challenge/recursion-sample.js
+++ javascript-challenge/recursion-sample.js
@@ -31,4 +31,18 @@ function power(num1, num2) {
 }
 console.log(power(2, 3)); //8
 
+/**
+ * Write a function that returns the factorial of a number. As a quick refresher, a factorial
+ * of a number is the result of that number multiplied by the number before it, and the
+ * number before that number, and so on, until you reach 1. The factorial of 1 is just 1
+ * Sample:
+ * factorial(5); // 5 * 4 * 3 * 2 * 1 === 120
+ */
+function factorial(num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorial(num - 1);
 
+}
+console.log(factorial(4));
