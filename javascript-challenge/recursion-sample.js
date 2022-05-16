2c889ca6c281b6ef4a8fdb10d47bbb58602134b0
Shirin Mohajer <shirinm@Shirins-MacBook-Pro.local>
2022-05-16 11:41:45 -0400

    add function power

--- javascript-challenge/recursion-sample.js
+++ javascript-challenge/recursion-sample.js
@@ -23,6 +23,12 @@ console.log(sumRange(3));
  * console.log(power(2, 0)); // 1
  */
 
-function power() {
-
+function power(num1, num2) {
+    if (num2 === 0) {
+        return 1;
+    }
+    return num1 * power(num1, num2 - 1);
 }
+console.log(power(2, 3)); //8
+
+
